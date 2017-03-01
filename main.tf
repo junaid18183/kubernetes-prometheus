67f445f196c2b799a2c2a13c27be77f33fda9e09
Juned Memon <junaid18183@gmail.com>
2017-03-01 13:08:08 +0000

    removed provider.tf and using single consolidated manifest main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "kubernetes" {
+    endpoint = "http://127.0.0.1:8001"
+#    Either local proxy or remote API with certs
+#    endpoint = "https://xxx.xxx.xxx.xxx"
+#    ca_cert = "ca.pem"
+#    client_cert = "admin.pem"
+#    client_key = "admin-key.pem"
+}
 #----------------------------------------------------------------
 resource "kubernetes_namespace" "monitoring" {
    name = "monitoring"
--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "kubernetes" {
-    endpoint = "http://127.0.0.1:8001"
-#    Either local proxy or remote API with certs
-#    endpoint = "https://xxx.xxx.xxx.xxx"
-#    ca_cert = "ca.pem"
-#    client_cert = "admin.pem"
-#    client_key = "admin-key.pem"
-}
-#----------------------------------------------------------------
